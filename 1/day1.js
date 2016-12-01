a8e264c4ac156e4835bb70386ced69a2440ef263
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-01 12:24:33 +0100

    Cleaned up and made part 2

--- 1/day1.js
+++ 1/day1.js
@@ -13,7 +13,10 @@ function read(file, callback) {
 read(args[0], function(data) {
     var dirs = data.split(",");
     var compass = 0; // 0 N, 1E, 2S, 3W
+    var c = [[0,1], [1,0], [0,-1], [-1,0]];
     var pos = [0,0];
+    var visited = Array.matrix(200,200,false);
+    var revisit = false;
 
     for(var index in dirs) {
         var v = dirs[index].trim();
@@ -28,26 +31,54 @@ read(args[0], function(data) {
             compass++;
             compass = compass>3?0:compass;
         }
-        switch(compass) {
-            case 0:
-                pos[1] += l; // N
-                break;
-            case 1:
-                pos[0] += l; // E
-                break;
-            case 2:
-                pos[1] -= l; // S
-                break;
-            case 3:
-                pos[0] -= l; // W
-                break;
+
+        var move = c[compass];
+
+        // Mark it
+        for(var i=0; i<l && !revisit; i++) {
+            var x = pos[0] + i*move[0];
+            var y = pos[1] + i*move[1];
+            revisit = checkVisit(x,y,visited);
+            visited[x][y] = true;
         }
+
+        // Move it
+        pos[0] += l * move[0];
+        pos[1] += l * move[1];
+
     }
 
-    // Calc using taxicab distance formula
-    var x1 = pos[0];
-    var y1 = pos[1];
-    var distance = Math.abs(x1 - 0) + Math.abs(y1 - 0);
+    if(!revisit) {
+        // Calc using taxicab distance formula
+        var x1 = pos[0];
+        var y1 = pos[1];
+        var distance = Math.abs(x1 - 0) + Math.abs(y1 - 0);
 
-    console.log('Distance to Easter Bunny HQ: ' + distance);
+        console.log('Distance to Easter Bunny HQ: ' + distance);
+    }
 });
+
+function checkVisit(x,y, visited) {
+    if(visited[x][y] == true) {
+        console.log('REVISIT at ' + x + ', ' + y);
+
+        // Calc using taxicab distance formula
+        var distance = Math.abs(x - 0) + Math.abs(y - 0);
+
+        console.log('Distance to revisited Easter Bunny HQ: ' + distance);
+        return true;
+    }
+    return false;
+}
+
+Array.matrix = function(numrows, numcols, initial) {
+    var arr = [];
+    for (var i = -numrows; i < numrows; ++i) {
+        var columns = [];
+        for (var j = -numcols; j < numcols; ++j) {
+            columns[j] = initial;
+        }
+        arr[i] = columns;
+    }
+    return arr;
+}
