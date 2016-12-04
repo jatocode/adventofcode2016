ad3c485ecdf91fc28984f9506f3dbf2c027efb23
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-04 12:09:44 +0100

    Feels like lots of object->array->object conv. could be done better

--- 4/advent.js
+++ 4/advent.js
@@ -11,7 +11,59 @@ function read(file, callback) {
 }
 
 read(args[0], function(data) {
- 
+	var testSectorIDsum = 1514;
+    
+    var lines = data.split("\n");
+	var idSum = 0;
+
+	for(var index in lines) {
+		var room = lines[index];
+
+		if(room.length == 0) break;
+
+		room = room.replace(/-/g,''); // Who cares about dashes
+		var regexp = /([a-z-]+)(\d+)\[(.*)\]/;
+		var roomdata = room.match(regexp);
+		var encrypted = roomdata[1];
+		var ID = parseInt(roomdata[2]);
+		var checksum = roomdata[3];
+
+		var count = {};
+		encrypted.split('').forEach(function(s) {
+			count[s] ? count[s]++ : count[s] = 1;
+		});
+
+		// Vill ha en array för att sortera
+		var ca = [];
+		Object.keys(count).forEach(function(k) {
+			var letter = [];
+			letter.push(k);
+			letter.push(count[k]);
+			ca.push(letter);
+		});
+		// Sortera på siffra och sen alfabet
+		ca.sort(function freq(a,b) {
+			if(a[1] < b[1]) return 1; // Antal först
+			if(a[1] > b[1]) return -1;
+			if(a[0] < b[0]) return -1; // Sen efter alfabet
+			if(a[0] > b[0]) return 1;
+			return 0;
+		});
+		
+		// Gör sträng av första fem?
+		var code = '';
+		for(var i=0;i<5;i++) {
+			code += ca[i][0];
+		}
+		//console.log(encrypted + ': ' + checksum + ', ' + ID + ', ' + JSON.stringify(ca) + ' = '+ code);
+
+		if(code == checksum) {
+			idSum += ID;
+		}
+
+	}
+
+	console.log('Sector ID sum = ' + idSum);
 
 });
 
