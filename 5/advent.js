e1b15b16434f2f460b6b0088125a0e8c5d38947b
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-05 10:40:43 +0100

    Part 2

--- 5/advent.js
+++ 5/advent.js
@@ -6,16 +6,26 @@ var doorID = 'ffykfhsq';
 
 var index = 0;
 var found = 0;
-var password = '';
+var password = new Array(8);
 
 while(found < 8) {
 
     var md5 = md5hex(doorID + index++);
 
     if(md5.startsWith('00000')) {
-        found++;
-        password += md5[5];
-        console.log(md5);
+        var pos = md5[5];
+        if(pos < 8 && password[pos] == undefined) {
+            password[pos] = md5[6];
+            found++;
+            var hollywood = '';
+            for(var i=0;i<password.length; i++) {
+                var l = password[i];
+                if(l == undefined) 
+                    hollywood += '_'
+                else
+                    hollywood += l;
+            }
+            console.log(hollywood);
+        }
     }
 }
-console.log('Password: ' + password);
