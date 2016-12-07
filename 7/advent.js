b72b53ccd059c78cb01bc31cd928e2e6645c1c65
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-07 16:28:16 +0100

    Part 2. Stupid stupid micro-optimization stumped me :(

--- 7/advent.js
+++ 7/advent.js
@@ -13,6 +13,7 @@ function read(file, callback) {
 read(args[0], function(data) {
     var lines = data.split("\n");
 
+    var ssl = 0;
     var tls = 0;
     for(var line in lines) {
         var ip7 = lines[line];
@@ -45,7 +46,49 @@ read(args[0], function(data) {
         if(abbaInIP && !abbaInHypernet) {
             tls++;
         }
+
+        // Part 2
+        var abas = [];
+        start = stop = hstart = hstop = 0;
+        for(var i=0;i<ip7.length;i++) {
+            if(ip7[i] == '[') {
+                stop = i;
+                var aba = checkABA(ip7.substring(start, stop));
+                if(aba.length > 0) {
+                    abas = abas.concat(aba);
+                }   
+            }
+            if(ip7[i] == ']') {
+                start = i+1;
+            }
+        }
+        if(start < ip7.length) {
+            var aba = checkABA(ip7.substring(start, ip7.length+1));
+            if(aba.length > 0)
+                abas = abas.concat(aba);
+        }
+
+        start = stop = hstart = hstop = 0;
+        var sslFound = false;
+        for(var i=0;i<ip7.length;i++) {
+            if(ip7[i] == '[') {
+                hstart = i+1;
+            }
+            if(ip7[i] == ']') {
+                hstop = i;
+                for(a in abas) {
+                    var aba = abas[a];
+                    if(checkBAB(ip7.substring(hstart,hstop), aba)) {
+                        sslFound = true;
+                    }
+                }
+            }
+        }
+        if(sslFound) {
+            ssl++;
+        }
     }
+    console.log('Number of IP that supports SSL: ' + ssl );
     console.log('Number of IP that supports TLS: ' + tls);
 
 });
@@ -67,3 +110,25 @@ function checkABBA(data) {
     return false;
 }
 
+function checkABA(data) {
+    if(data == null || data == undefined) return [];
+    var aba = [];
+    for(var j=0;j<data.length && j+2 < data.length;j++) {
+        if((data[j] == data[j+2]) && (data[j] != data[j+1])) {
+            aba.push(data[j]+data[j+1]+data[j+2]);
+        }
+    }
+    return aba; 
+}
+
+function checkBAB(data, aba) {
+    if(data == null || data == undefined) return false
+        for(var j=0;j<data.length && j+2 < data.length;j++) {
+            var bab = data[j] + data[j+1] + data[j+2];
+            var caba = aba[1] + aba[0] + aba[1];
+            if(bab === caba) {
+                return true;
+            }
+        }
+    return false
+}
