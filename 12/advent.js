6ee8d67545be48918866cab28dd3d503d73c628e
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-12 12:51:22 +0100

    Part 1 & 2

--- 12/advent.js
+++ 12/advent.js
@@ -1,7 +1,10 @@
 var fs = require('fs');
 var args = process.argv.slice(2);
 
-var display;
+var instructions = [];
+var regs = [];
+regs['d'] = regs['c'] = regs['b'] = regs['a'] = 0;
+regs['c'] = 1;
 
 function read(file, callback) {
     fs.readFile(file, 'utf8', function(err, data) {
@@ -15,9 +18,63 @@ function read(file, callback) {
 read(args[0], function(data) {
     var lines = data.split("\n");
 
-    for(var l=0; l < lines.length ; l++) {
+    for(var l in lines) {
         var line = lines[l];
-
+        var f = line.match(/([a-z]+) (\d+|[a-d]).?(-?\d+|[a-d])?/);
+        if(f) {
+            var inst = {};
+            inst.type = f[1];
+            inst.data = [f[2], f[3]];
+            instructions.push(inst);
+        }
     }
+    var i=0;
+    while(i < instructions.length) {
+        var inst = instructions[i];
+//        console.log(i + ' : ' + inst.type + ':' + inst.data + 
+//            ' a=' + regs['a'] + ' b=' + regs['b'] + ' c=' + regs['c'] + ' d=' + regs['d']);
+        switch(inst.type) {
+            case 'cpy':
+                 var c = inst.data[0]; 
+                var ch = c.match(/[a-d]+/);
+               if(ch) {
+                    c = regs[c];
+                } else {
+                    c = parseInt(c);
+                }           
+                regs[inst.data[1]] = c;
+                i++;
+                break;
+            case 'inc':
+                regs[inst.data[0]]++;         
+                i++;       
+                break;
+            case 'dec':
+                regs[inst.data[0]]--;          
+                i++;      
+                break;
+            break;
+            case 'jnz':
+                var c = inst.data[0]; 
+                var s = parseInt(inst.data[1]);
+                var ch = c.match(/[a-d]+/);
+               if(ch) {
+                    c = regs[c];
+                } else {
+                    c = parseInt(c);
+                }
+
+                if(c != 0) {
+                    i += s;
+                } else {
+                    i++;
+                }
+                break;
+            default:
+                console.log('Monorail failure');
+                break;
+        }
+    }
+    console.log(regs);
 });
 
