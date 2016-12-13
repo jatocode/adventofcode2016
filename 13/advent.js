1ed8aa13f7dc5817edf1df3891888a2781c4485a
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-13 13:55:08 +0100

    Finds path but not shortest

--- 13/advent.js
+++ 13/advent.js
@@ -1,33 +1,132 @@
 var args = process.argv.slice(2);
 
 var fav = 1352;
-var x = 31;
-var y = 39;
+var destx = 31;
+var desty = 39;
 
 if(args.length > 0) {
-    fav = args[0];
-    x = args[1];
-    y = args[2];
+    fav = parseInt(args[0]);
+    destx = parseInt(args[1]);
+    desty = parseInt(args[2]);
 }
 
-console.log('Steps needed to reach (' + x + ',' + y + ') with fav ' + fav +' : ' + maze(x, y));
+var steps = 0;
 
-function maze(destX, destY) {
-    isWall(0,1);
+// Implement simple pathfinder
+var queue = [];
+queue.push({x:0,y:0,w:0});
+function maze(q) {
+    var x = q.x;
+    var y = q.y;
+    var w = parseInt(q.w);
+    if(x == destx && y == desty) {
+        console.log('FF');
+        return true;
+    }
 
-    return 0;
+    var nbs = [{x:x+1, y:y,   w:w+1},
+    {x:x,   y:y+1, w:w+1},
+    {x:x-1, y:y,   w:w+1},
+    {x:x,   y:y-1, w:w+1}];
+
+    for(n in nbs) {
+        var nb = nbs[n];
+        if (nb.x < 0 || nb.y < 0) {
+            //   console.log('Negative: ' + JSON.stringify(nb));
+        } else if(isWall(nb.x, nb.y)) {
+            //   console.log('isWall:' + JSON.stringify(nb));
+        } else if(inQueueAndHeavy(nb)) {
+            // Find nb in queue and compare weight
+          //  console.log('Heavy: ' + JSON.stringify(nb));
+        } else {
+            addToQ(nb);
+        }
+    }
+
+    return false;
 }
 
+function find() {
+    var steps = 0;
+    var found = false;
+    var i = 0;
+    while(!found && steps++<40) {
+        for(i in queue) {
+            var q = queue[i];
+            found = maze(q);
+            if(found) break;
+        }
+        display(q.x, q.y);
+    }
+    return steps;
+}
+
+display(0,0);
+console.log('Steps needed to reach (' + destx + ',' + desty + ') with fav ' + fav +' : ' + find());
+
+function addToQ(c) {
+    var f = false;
+    for(var i in queue) {
+        var p = queue[i];
+        if(p.x == c.x && p.y == c.y) {
+            f = true;
+            if(c.w >= p.w) {
+                queue[i].w = c.w;
+            }
+            return;
+        }
+    }
+    if(!f) queue.push(c);
+    return;
+}
+function inQueueAndHeavy(c) {
+    for(var i in queue) {
+        var p = queue[i];
+        if(p.x == c.x && p.y == c.y && (p.w >= c.w)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function display(a,b) {
+//    console.log(queue);
+//    console.log();
+//    console.log(a,':',b);
+//    return;
+    for(var y=0;y<7;y++) {
+        var row = '';
+        for(var x=0;x<10;x++) {
+            var w = 'O';
+            for(i in queue) {
+                var q = queue[i];
+                if(x == q.x && y == q.y) {
+                    a = q.x;
+                    b = q.y;
+                 //   w = q.w;
+                    break;
+                }
+            }
+            if(x == a && y == b) {
+                row += w;
+            } else {
+                row += isWall(x,y)?'#':'.';
+            }
+        }
+        console.log(row);
+    }
+    console.log();
+}
 
 function isWall(x, y) {
     var f = x*x + 3*x +2*x*y + y + y*y;
     f = f + fav;
-    
-    var count = krCount(f);
-//    console.log(f + ': ' + f.toString(2) + ' -> ' + count);
-    var wall = count % 2;
 
-    console.log('(' + x + ',' + y + ') is ' + (wall==0?'an open space':'a wall'));
+    var count = krCount(f);
+    // console.log(f + ': ' + f.toString(2) + ' -> ' + count);
+    var wall = (count % 2) != 0;
+
+    //console.log('(' + x + ',' + y + ') is ' + (wall?'an open space':'a wall'));
     return wall;
 
 }
