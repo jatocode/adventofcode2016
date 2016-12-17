c8d2dec94b5a535b5b8451a0bdb0d2788d5aff39
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-17 10:16:38 +0100

    Fix

--- 11/advent.js
+++ 11/advent.js
@@ -23,21 +23,24 @@ read(args[0], function(data) {
         floors[l] = parseFloor(line);
     }
 
-    displayFloors();
+   // console.log(JSON.stringify(floors));
+   displayFloors(4);
 
 });
 
-function displayFloors() {
+
+
+function displayFloors(size) {
    for(var f=floors.length-1; f >= 0; f--) {
         var fl = 'F'+(f+1)+' ';
         var all = floors[f][0].concat(floors[f][1]);
-        for(var i=0; i < 10; i++) {
+        for(var i=0; i < size; i++) {
             var u = all[i] == undefined?'  . ':' ' + all[i] + ' ';
             fl += u;
         }
         console.log(fl);
     }
-    console.log('H: ' + hiss + ' (' + steps +')');
+    //console.log('H: ' + hiss + ' (' + steps +')');
 }
 
 function parseFloor(line) {
@@ -59,21 +62,34 @@ function parseFloor(line) {
     if(line.indexOf('cobalt generator') != -1) {
         generators.push('CG');
     }
+    if(line.indexOf('hydrogen generator') != -1) {
+        generators.push('HG');
+    }
+    if(line.indexOf('lithium generator') != -1) {
+        generators.push('LG');
+    }
 
     if(line.indexOf('polonium-compatible') != -1) {
-        microchips.push('PC');
+        microchips.push('PM');
     }
     if(line.indexOf('thulium-compatible') != -1) {
-        microchips.push('TC');
+        microchips.push('TM');
     }
     if(line.indexOf('promethium-compatible') != -1) {
-        microchips.push('OC');
+        microchips.push('OM');
     }
     if(line.indexOf('ruthenium-compatible') != -1) {
-        microchips.push('RC');
+        microchips.push('RM');
     }
     if(line.indexOf('cobalt-compatible') != -1) {
-        microchips.push('CC');
+        microchips.push('CM');
     }   
+    if(line.indexOf('hydrogen-compatible') != -1) {
+        microchips.push('HM');
+    }   
+    if(line.indexOf('lithium-compatible') != -1) {
+        microchips.push('LM');
+    }   
+
     return [generators, microchips]; 
 }
