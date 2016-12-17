84e2617233c8ae892a0018b4cfdb174c3fcc7b27
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-17 18:42:59 +0100

    Works and is clean

--- 17/advent.js
+++ 17/advent.js
@@ -13,38 +13,47 @@
 */
 var md5hex = require('md5-hex');
 
-var passcode = 'qzthpkfp';
-//passcode = 'hijkl';
-var current = [0,0];
+var passcodes = ['hijkl', 'ihgpwlah', 'kglvqrro', 'ulqzkmiv', 'qzthpkfp'];
+var expected = ['', 'DDRRRD', 'DDUDRLRRUDRD', 'DRURDRUDDLLDLUURRDULRLDUUDDDRR', ''];
 
-console.log('The path to the vault is : ' + findVault());
+for(var p in passcodes) {
+	var passcode = passcodes[p];
 
-function findVault() {
-	return breadthFirst({xy:[0,0]}, [3,3], 5000);
+	var result = breadthFirst({xy:[0,0]}, [3,3], 5000);
+	console.log('The path to the vault with start ' + passcode + ' is : ' +  result);
+	if(expected[p] != '' && expected[p] != result) {
+		console.log('Expected ' + expected[p] + ' got ' + result);
+	}
+	console.log();
 }
 
+
 function openPath(x, y, path, lastStep) {
-	//console.log(x + ',' + y + ' ' + path + ' ' + lastStep);
 	if(x < 0 || y < 0 || x > 3 || y > 3) {
 		return false;
 	}
 
-	// Path minus förra steget borde det bli??
 	var test;
 	if(path.length > 0 ) {
-		test = md5hex(passcode + path.substring(0, path.length -1) ).substring(0,4);
+		test = md5hex(passcode + path).substring(0,4);
 	} else {
 		test = md5hex(passcode).substring(0,4);		
 	}
-	var dirs = {D:0, U:1, L:2, R:3};
+	var dirs = {U:0, D:1, L:2, R:3};
 	var k = test[dirs[lastStep]];
+
+	var open = false;
 	if(k.match(/[bcdef]/)) {
-		return true;
+		open = true;
 	}
-	return false;
+	//console.log(x + ',' + y + ' ' + passcode + path + '( ' + test + '), ' + path + ', ' + lastStep + ' -> ' + open);
+
+	return open;
 }
 
 function breadthFirst(root, end, maxSteps) {
+	var current = [0,0];
+	var reached = [];
 
 	destx = end[0];
 	desty = end[1];
@@ -57,27 +66,17 @@ function breadthFirst(root, end, maxSteps) {
 	Q.push(root);
 	var steps = 0;
 	while(Q.length > 0 && steps++<maxSteps) {
-		console.log(Q.length);
 		var current = Q.shift();
 
-		// Are we there yet?
 		if(current.xy[0] == destx && current.xy[1] == desty) {
-			// Found destination. Backtrace!
-			console.log('Reached ' + current.xy);
-			//printDestination(destx, desty);
-			return current.path;
+			reached.push(current);
 		}
 
-		// if(inQueue(current, V)) {
-		// 	// Already visited
-		// 	continue;
-		// }
-		// V.push(current);
 		var neighb = [ 
-		{xy:[current.xy[0],   current.xy[1]+1], lastStep: 'D' },
-		{xy:[current.xy[0],   current.xy[1]-1], lastStep: 'U' },
-		{xy:[current.xy[0]+1, current.xy[1]],   lastStep: 'R' },
-		{xy:[current.xy[0]-1, current.xy[1]],   lastStep: 'L' },
+			{xy:[current.xy[0],   current.xy[1]+1], lastStep: 'D' },
+			{xy:[current.xy[0],   current.xy[1]-1], lastStep: 'U' },
+			{xy:[current.xy[0]+1, current.xy[1]],   lastStep: 'R' },
+			{xy:[current.xy[0]-1, current.xy[1]],   lastStep: 'L' },
 		];
 
 		for(i in neighb) {
@@ -99,16 +98,22 @@ function breadthFirst(root, end, maxSteps) {
 			}
 		}
 	}
-	if(steps > maxSteps) {
-		console.log('Number of visited: ' + V.length);
+
+	reached.sort(function(a,b) {
+		return a.length - b.length;
+	});
+
+	if(reached.length > 0) {
+		return reached[0].path;
 	}
-	// console.log('Not found after ' + steps);
-	return -1;
+	return [];
 }
 
 function inQueue(v, V) {
     for(i in V) {
-        if(V[i].xy[0] == v.xy[0] && V[i].xy[1] == v.xy[1]) {
+        if(V[i].xy[0] == v.xy[0] && 
+        	V[i].xy[1] == v.xy[1] && 
+        	V[i].path == v.path) {
             return V[i];
         }
     }
