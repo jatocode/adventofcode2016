a4553a42e4c1a41ae259b74334eb9bcffcad223c
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-18 10:17:55 +0100

    Boring but started anyway

---
+++ 18/advent.js
@@ -0,0 +1,24 @@
+var startrow = '.^^^^^.^^.^^^.^...^..^^.^.^..^^^^^^^^^^..^...^^.^..^^^^..^^^^...^.^.^^^^^^^^....^..^^^^^^.^^^.^^^.^^';
+var safe = 0;
+var rooms = 40;
+
+var rows = [];
+
+for(i in startrow.split('')) {
+    var row = [];
+    if(startrow[i] == '.') {
+        row.push(1);
+    } else if(startrow[i] == '^') {
+        row.push(0)
+    }
+    rows.push(row);
+}
+
+for(var room=0; room < rooms; room++ ) {
+    for(r in rows[room]) {
+        var tile = rows[room][r];
+        safe += tile;
+    }
+}
+
+console.log('Safe tiles: ' + safe);
