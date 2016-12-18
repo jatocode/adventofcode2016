bcb984f9fa123b1e8d3a29688a4e7a442df6bbc8
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-18 14:48:32 +0100

    Easy

--- 18/advent.js
+++ 18/advent.js
@@ -1,30 +1,63 @@
 var startrow = '.^^^^^.^^.^^^.^...^..^^.^.^..^^^^^^^^^^..^...^^.^..^^^^..^^^^...^.^.^^^^^^^^....^..^^^^^^.^^^.^^^.^^';
+
+//var startrow = '..^^.';
+
 var safe = 0;
-var rooms = 40;
+var rooms = 400000;
+var SAFE = 1;
+var TRAP = 0;
 
 var rows = [];
 
+var row = [];
 for(i in startrow.split('')) {
-    var row = [];
     if(startrow[i] == '.') {
-        row.push(1);
+        row.push(SAFE);
     } else if(startrow[i] == '^') {
-        row.push(0)
+        row.push(TRAP)
     }
-    rows.push(row);
 }
+rows.push(row);
 
 for(var room=0; room < rooms; room++ ) {
-    for(r in rows[room]) {
+    if(rows[room] == undefined) {
+         rows[room] = [];
+    }
+    //console.log(room + ' ' + JSON.stringify(rows[room]));
+    for(var ti=0; ti < rows[0].length ; ti++) {
         var tile;
         if(room == 0) {
-            tile = rows[room][r];
+            tile = rows[room][ti];
             safe += tile;
-        } else {
-            // LOGIC
+        } else {            
+            left = rows[room-1][parseInt(ti) - 1];
+            if(left == undefined) left = SAFE;
 
+            center = rows[room-1][parseInt(ti)];
+
+            right = rows[room-1][parseInt(ti) + 1];
+            if(right == undefined) right = SAFE;
+
+            tile = SAFE;
+            if(left == TRAP && center == TRAP && right == SAFE) {
+                tile = TRAP;
+            } 
+            if (left == SAFE && center == TRAP && right == TRAP) {
+               tile = TRAP;
+            } 
+            if (left == TRAP && center == SAFE && right == SAFE) {
+                tile = TRAP;
+            } 
+            if (left == SAFE && center == SAFE && right == TRAP) {
+                tile = TRAP;
+            } 
+
+            //console.log('Index ' + ti + ' ' + left + ' '+ center + ' ' + right + ' ' + (tile==SAFE?'SAFE':'TRAP'));
+            safe += tile;
+            rows[room].push(tile);
         }
     }
+    //console.log(room + ' ' + JSON.stringify(rows[room]));
 }
 
 console.log('Safe tiles: ' + safe);
