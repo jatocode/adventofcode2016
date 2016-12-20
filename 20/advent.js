dec639d96a1396ec2cb4e7a74fcc0637f92b22e6
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-20 23:54:05 +0100

    Part 2. Not sure why it works, but it does

--- 20/advent.js
+++ 20/advent.js
@@ -32,9 +32,9 @@ read(args[0], function(data) {
 
     var range = 0;
     var ip = 0;
-    var possible = [];
     var n = 0;
     var lowest = 0;
+    var whitelisted = 0;
     for(r in ranges) {
         var start = ranges[r].start;
         var end = ranges[r].end;
@@ -42,13 +42,18 @@ read(args[0], function(data) {
         if(n < start) {
             if(lowest == 0) {
                 lowest = n;
-                break; // Found it
+                //break; // Found it
             }
+            whitelisted += (start - n);
+            console.log(whitelisted);
         }
         if(n <= end) {
             n = end + 1;
         }
     }
+    console.log(n);
+    console.log(whitelisted);
+    whitelisted += (4294967296 - n);
     console.log(lowest);
-
+    console.log(whitelisted);
 });
