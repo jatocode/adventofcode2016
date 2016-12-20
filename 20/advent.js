db011462872c3583a9cc75b14c2101eb6be46c5d
Tobias Jansson <tobias.jansson@gmail.com>
2016-12-20 23:23:15 +0100

    Part 1

---
+++ 20/advent.js
@@ -0,0 +1,54 @@
+var fs = require('fs');
+var args = process.argv.slice(2);
+
+function read(file, callback) {
+    fs.readFile(file, 'utf8', function(err, data) {
+        if (err) {
+            console.log(err);
+        }
+        callback(data);
+    });
+}
+
+read(args[0], function(data) {
+    var lines = data.split("\n");
+
+    var ranges = [];
+    var ends= [];
+    var starts = [];
+    for(var l=0; l < lines.length ; l++) {
+        var line = lines[l];
+        var range = line.match(/(\d+)-(\d+)/);
+        if(range) {
+            var range = {start:parseInt(range[1]), end:parseInt(range[2])};
+            ranges.push(range);
+            starts.push(range.start);
+            ends.push(range.end);
+        }
+    }
+    ranges.sort(function(a,b) {
+        return a.start - b.start;
+    });
+
+    var range = 0;
+    var ip = 0;
+    var possible = [];
+    var n = 0;
+    var lowest = 0;
+    for(r in ranges) {
+        var start = ranges[r].start;
+        var end = ranges[r].end;
+        console.log(n + ' ' + start + ' ' + end + ' ' + lowest);
+        if(n < start) {
+            if(lowest == 0) {
+                lowest = n;
+                break; // Found it
+            }
+        }
+        if(n <= end) {
+            n = end + 1;
+        }
+    }
+    console.log(lowest);
+
+});
