0fb96ab66f524dcfbccf970ace889f73015fdda8
Tobias Jansson <tobias.jansson@gmail.com>
2017-01-04 13:25:42 +0100

    Cleanup. Ready for new algorithm

--- helpers.js
+++ helpers.js
@@ -1,6 +1,6 @@
-var start = {xy:[1,1]};
-var end = {xy:[7,4]};
-
+// Test functions
+var start = Node(1,1);
+var end = Node(7,4);
 var testArray = [
     '.#.####.##',
     '..#..#...#',
@@ -11,22 +11,9 @@ var testArray = [
     '#...##.###'
 ];
 
-var shortestPath = [ [ 7, 4 ],
-    [ 6, 4 ],
-    [ 6, 5 ],
-    [ 5, 5 ],
-    [ 4, 5 ],
-    [ 4, 4 ],
-    [ 3, 4 ],
-    [ 3, 3 ],
-    [ 3, 2 ],
-    [ 2, 2 ],
-    [ 1, 2 ] ];
+var shortestPath = [ [ 7, 4 ], [ 6, 4 ], [ 6, 5 ], [ 5, 5 ], [ 4, 5 ], [ 4, 4 ], [ 3, 4 ], [ 3, 3 ], [ 3, 2 ], [ 2, 2 ], [ 1, 2 ] ];
 
-//console.log(testArray);
-//console.log(testArray.length + ' ' + testArray[0].length);
-
-var path = breadhfirst(start, end, pathCheckTest);
+var path = breadthfirst(start, end, pathCheckTest);
 if(path.length < 0) {
     console.log('Path finder failed');
 }
@@ -36,24 +23,19 @@ if(comparePath(path, prettypath)) {
 }
 console.log('Path length: ' + path.length);
 
-function breadhfirst(root, end, possiblePath) {
-
-    destx = end.xy[0];
-    desty = end.xy[1];
+// Breadth First
+function breadthfirst(root, end, possiblePath) {
 
     console.log('BFS, looking for path from ' + root.xy + ' to ' + end.xy);
 
     var Q = [];
     var V = [];
     root.distance = 0;
-
     Q.push(root);
-    var steps = 0;
+
     while(Q.length > 0) {
         var current = Q.shift();
-        //        console.log(current);
-        if(current.xy[0] == destx && current.xy[1] == desty) {
-            // Found destination. Backtrace!
+        if(current.x() == end.x() && current.y() == end.y()) {
             var path = [];
             while(current.parent != undefined) {
                 path.push(current);
@@ -66,24 +48,17 @@ function breadhfirst(root, end, possiblePath) {
             continue;
         }
         V.push(current);
-        var neighb = [{xy:[current.xy[0],   current.xy[1]+1] },
-        {xy:[current.xy[0],   current.xy[1]-1] },
-        {xy:[current.xy[0]+1, current.xy[1]]   },
-        {xy:[current.xy[0]-1, current.xy[1]],  },
-        ];
-
+        var neighb = neighb4(current);
         for(i in neighb) {
             var n = neighb[i];
-            var nx = n.xy[0];
-            var ny = n.xy[1];
-
             if(inQueue(n, Q)) {
                 continue;
             }
-            if(nx >= 0 && ny >= 0 && possiblePath(nx, ny)) {             
+            if( possiblePath(n.x(), n.y()) ) {             
                 if(n.distance == undefined ) {
                     n.distance = current.distance + 1;
-                    n.parent = {xy:[current.xy[0], current.xy[1]], distance:current.distance};
+                    n.parent = Node(current.x(), current.y());
+                    n.parent.distance = current.distance;
                     Q.push(n);
                 }
             }
@@ -93,19 +68,34 @@ function breadhfirst(root, end, possiblePath) {
     return -1;
 }
 
-function checkPath(x, y) {
-    return true;
-}
-
 function inQueue(v, V) {
     for(i in V) {
-        if(V[i].xy[0] == v.xy[0] && V[i].xy[1] == v.xy[1]) {
+        if( V[i].x() == v.x() && V[i].y() == v.y() ) {
             return V[i];
         }
     }
     return 0;
 }
 
+function neighb4(node) {
+    return [Node(node.x(),     node.y() + 1 ), 
+            Node(node.x(),     node.y() - 1 ),
+            Node(node.x() + 1, node.y() ),
+            Node(node.x() - 1, node.y() ) ];
+}
+
+function Node(x, y) {
+    var n = { xy: [x,y],
+              distance: undefined, 
+              g: undefined, 
+              f: undefined,
+              x: function() { return this.xy[0]; },
+              y: function() { return this.xy[1]; },
+            };
+    return n;
+}
+
+// For testing
 function prettyPrintPath(path) {
     var prettyp = [];
     for(p in path) {
@@ -126,7 +116,7 @@ function comparePath(a, b) {
 
 function pathCheckTest(x, y) {
     //console.log(y + ': ' + testArray[y]);
-    if(y > testArray.length - 1 || x > testArray[y].length - 1) {
+    if(x < 0 || y < 0 || y > testArray.length - 1 || x > testArray[y].length - 1) {
         return false;
     }
     var xdata = testArray[y].split('');
